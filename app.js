29d3618fcf224d98cb9c703da5e9d0f866cdcea4
Jim Padilla <jp_code2013@yahoo.com>
2018-03-25 20:27:13 -0700

    Remove sample middleware example

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const express = require('express'); // Calling a module, like RoR require 'somet
 const app = express();
 const port = 5000;
 
-app.use(function(req, res, next) { // Simple piece of middleware
-  console.log(Date.now());
-  req.name = "James P";
-  next();
-});
-
 // Index Route
 app.get('/', (request, response) => {
-  console.log(request.name); // Simple use of middleware var
   response.send('INDEX');
 });
 
