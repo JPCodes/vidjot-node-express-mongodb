888a09ca352dfe92eec8bc61d9060be72f8089b5
Jim Padilla <jp_code2013@yahoo.com>
2018-03-25 20:35:37 -0700

    Include Handlebars middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express'); // Calling a module, like RoR require 'something'
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 5000;
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (request, response) => {
